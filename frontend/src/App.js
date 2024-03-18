5f3abaf1a5507c0eb76e1bd42ff12f8c0128bdba
ethankonk <ethan.konkolowicz@gmail.com>
2024-03-18 08:57:00 -0400

    fixed login page css when screen size small

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 
 import Footer from './components/Footer';
 import Main from './components/Main'
+
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
 
 function App() {
+  const navigate = useNavigate()
+  useEffect(()=>navigate('/'), [])
 
   return (
     <div className="App">
