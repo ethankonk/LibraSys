6d563ca0f972489659cef5d740203a299f00a544
ethan.konk <ethan.konkolowicz@gmail.com>
2024-03-30 11:04:53 -0400

    added line graph too admin page

--- frontend/src/components/BorrowGraph.js
+++ frontend/src/components/BorrowGraph.js
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
 import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
 
 const BooksBorrowedGraph = () => {
   const [chartData, setChartData] = useState([null]);
-  const sampleData = {
-    labels: ['2022-01-01', '2022-02-01', '2022-03-01', '2022-04-01', '2022-05-01'],
-    datasets: [
-        {
-            label: 'Books Borrowed Over Time',
-            data: [10, 20, 15, 25, 30], // Sample counts of books borrowed
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1,
-        },
-    ],
-  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,41 +38,30 @@ const BooksBorrowedGraph = () => {
         }
     };
 
+    console.log(chartData)
+
     fetchData();
   }, []);
 
   return (
-    <div>
-      {chartData && (
-        <Line
-          data={chartData}
-          options={{
-            scales: {
-              x: {
-                type: 'time',
-                time: {
-                  unit: 'day',
-                  displayFormats: {
-                    day: 'MMM D',
-                  },
-                },
-                title: {
-                  display: true,
-                  text: 'Date',
-                },
-              },
-              y: {
-                title: {
-                  display: true,
-                  text: 'Number of Books',
-                },
-                beginAtZero: true,
-              },
-            },
-          }}
-        />
-      )}
-    </div>
+    <Line
+    datasetIdKey='id'
+    data={{
+        labels: ['Jun', 'Jul', 'Aug'],
+        datasets: [
+        {
+            id: 1,
+            label: 'Dataset 1',
+            data: [5, 6, 7],
+        },
+        {
+            id: 2,
+            label: 'Dataset 2',
+            data: [3, 2, 1],
+        },
+        ],
+    }}
+    />
   );
 };
 
